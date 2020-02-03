66f9d36d9affc2e03c27fb335a23f894ffa5bd51
johnsooraj <john93john>
2020-02-03 22:28:15 +0530

    context-change

--- source-code/angular-portal-app/src/app/services/common.service.ts
+++ source-code/angular-portal-app/src/app/services/common.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Employee } from '../models/employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonService {
+
+  FETCH_ALL_EMPLOYEES = '/portal/employees';
+  ADD_NEW_EMPLOYEE = '/portal/employee';
+  FETCH_ATTENDANCE_LOG_BY_EMP_ID = '/portal/attendance/log';
+  CHECK_IN_EMPLOYEE = '/portal/employees';
+  CHECK_OUT_EMPLOYEE = '/portal/employees';
 
   constructor(
     private http: HttpClient
@@ -13,5 +20,9 @@ export class CommonService {
 
   getDummyData(): Observable<any> {
     return this.http.get('https://jsonplaceholder.typicode.com/posts?_start=0&_limit=5');
+  }
+
+  fetchAllEmployees(page: number, _limit: number): Observable<Employee[]> {
+    return this.http.get<Employee[]>('');
   }
 }
